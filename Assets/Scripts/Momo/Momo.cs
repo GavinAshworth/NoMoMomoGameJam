fca174498f9bf53fc3f17bdf5ba0f10da7c02442
GavinAshworth <gavinashworth315@gmail.com>
2025-03-18 21:39:07 -0700

    Implemented collision logic
    Momo now dies when he hits the abyss. Attaches to moving platforms. And can land safely on the homes

--- Assets/Scripts/Momo/Momo.cs
+++ Assets/Scripts/Momo/Momo.cs
@@ -5,6 +5,8 @@ public class Momo : MonoBehaviour
 {
     [SerializeField] private float moveDistance = 1f; // Set movement step size
     [SerializeField] private float moveTime = 0.2f;  // Time to complete movement
+    [SerializeField] private GameObject deathSprite;
+    [SerializeField] private Vector3 respawnPoint; // Momo's current reset position
     private Rigidbody2D rb;
     private Vector2 moveDirection;
     private bool isMoving = false; // Prevent multiple inputs before finishing move
@@ -27,7 +29,30 @@ public class Momo : MonoBehaviour
         if (moveDirection != Vector2.zero) //When user moves
         {
             Vector2 targetPosition = (Vector2)transform.position + moveDirection * moveDistance;
-            StartCoroutine(MoveToPosition(targetPosition));//This allows us to move smoothly instead of just teleporting, looks nicer
+
+            Collider2D platform = Physics2D.OverlapBox(targetPosition, Vector2.zero, 0f, LayerMask.GetMask("Platform"));
+            Collider2D abyss = Physics2D.OverlapBox(targetPosition, Vector2.zero, 0f, LayerMask.GetMask("Abyss"));
+            Collider2D home = Physics2D.OverlapBox(targetPosition, Vector2.zero, 0f, LayerMask.GetMask("Home")); //These are where momo is trying to get to in order to progress
+            Collider2D ground = Physics2D.OverlapBox(targetPosition, Vector2.zero, 0f, LayerMask.GetMask("Ground")); //non moving ground that momo is safe on
+            // If momo lands on a platfrom we attach him to it
+            if (platform != null) {
+                transform.SetParent(platform.transform);
+            } else {
+                transform.SetParent(null);
+            }
+            //some logic here for when momo sucessfully gets to a home
+            // if (home != null) {
+            //     Gamemanager.instance.fillHome or something like this;
+            // } 
+            // Momo dies when he lands in the abyss (loses a life and gets reset). If he is flying (using air ability we dont call this... which will get added later)
+            if (abyss != null && platform == null && ground == null && home == null)
+            {
+                //Call our death function. Currently everything just does 1 damage for now
+                Death(targetPosition, 1);
+            }else{
+                StopAllCoroutines();
+                StartCoroutine(MoveToPosition(targetPosition)); // This allows us to move smoothly instead of just teleporting, looks nicer
+            }
         }
     }
 
@@ -85,5 +110,46 @@ public class Momo : MonoBehaviour
         transform.position = pos;
 
         animator.SetBool("isJumping", false); //stops the jumping animation and returns to idle
+    }
+
+    public void Death(Vector2 target, int damage){
+        StopAllCoroutines();    
+        moveDirection = Vector2.zero;  //reset movement
+        isMoving = false; // we arent moving anymore
+        animator.SetBool("isJumping", false); //exit jump animation
+        transform.SetParent(null);
+        //Show hurt sprite at the destination tile
+        if(deathSprite!=null){
+            GameObject deathFeedback = Instantiate(deathSprite, target, Quaternion.identity);
+            //We will have it last for a couple seconds and then destroy it. Maybe later we will have it fade out
+            Destroy(deathFeedback, 2f);
+        }else{
+            Debug.Log("No death sprite attached to momo script");
+        }
+
+        //Reset Momo's abilities here in the future
+        //Disable control of this script so momo cant move during death
+        enabled = false;
+
+        //Disable momo so he cannot do anything while dead
+
+        gameObject.SetActive(false);
+
+        //Call our death function in the game manager once its set up
+        Invoke(nameof(Respawn), 1f);
+    }
+
+    public void Respawn(){
+        //This is called by our game manager after death, if we have more lives left. Right now we just call it in this file
+        StopAllCoroutines(); 
+
+        //Re enable momo
+        gameObject.SetActive(true);
+
+        //re-enable control
+        enabled = true;
+
+        //spawn momo at respawn point
+        transform.position = respawnPoint;
     }
 }
