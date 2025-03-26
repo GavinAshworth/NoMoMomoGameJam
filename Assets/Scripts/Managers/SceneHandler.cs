3bd54100e190e5f178fa5f332725f026021260d8
Jimi Ademola <jimi.ademola1@gmail.com>
2025-03-25 17:07:08 -0700

    SceneManager implemented for first level

---
+++ Assets/Scripts/Managers/SceneHandler.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : SingletonMonoBehavior<SceneHandler>
+{
+    
+    [Header("Scene Data")]
+    [SerializeField] private List<string> levels;
+    [SerializeField] private string menuScene;
+    [Header("Transition Animation Data")]
+    [SerializeField] private Ease animationType;
+    [SerializeField] private float animationDuration;
+    [SerializeField] private RectTransform transitionCanvas;
+
+    private int nextLevelIndex;
+    private float initXPosition;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected override void Awake()
+    {
+        base.Awake();
+        initXPosition = transitionCanvas.transform.localPosition.x;
+        SceneManager.LoadScene(menuScene);
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode _)
+    {
+        transitionCanvas.DOLocalMoveX(initXPosition, animationDuration).SetEase(animationType);
+    }
+
+    public void LoadNextScene()
+    {
+        if(nextLevelIndex >= levels.Count)
+        {
+            LoadMenuScene();
+        }
+        else
+        {
+            transitionCanvas.DOLocalMoveX(initXPosition + transitionCanvas.rect.width, animationDuration).SetEase(animationType);
+            StartCoroutine(LoadSceneAfterTransition(levels[nextLevelIndex]));
+            nextLevelIndex++;
+        }
+    }
+
+    public void LoadMenuScene() {
+        StartCoroutine(LoadSceneAfterTransition(menuScene));
+        nextLevelIndex = 0;
+    }
+
+    private IEnumerator LoadSceneAfterTransition(string scene)
+    {
+        yield return new WaitForSeconds(animationDuration);
+        SceneManager.LoadScene(scene);
+    }
+
+    private IEnumerator LoadInitialScene()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(menuScene);
+    }
+}
